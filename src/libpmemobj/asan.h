a5062d8c10ec746e15e890fd03267e36bc563ed9
Kartal Kaan Bozdoğan <kartalkaanbozdogan@gmail.com>
2021-06-15 18:17:29 +0300

    Move libpmemobj-dependent stuff from asan.h to asan_wrappers.h

--- src/libpmemobj/asan.h
+++ src/libpmemobj/asan.h
@@ -2,9 +2,7 @@
 #define PMEMOBJ_ASAN_COMMON_H
 
 #include <stdint.h>
-#include <stdlib.h>
-#include <libpmemobj/types.h>
-#include "tx.h"
+#include <stddef.h>
 
 #define pmemobj_asan_ADDRESSABLE 0
 #define pmemobj_asan_LEFT_REDZONE 0xFA
@@ -14,23 +12,6 @@
 #define pmemobj_asan_METADATA pmemobj_asan_INTERNAL // kartal TODO: Is this correct? What values does ASan use to represent metadata?
 
 #define pmemobj_asan_RED_ZONE_SIZE 128
-
-POBJ_LAYOUT_BEGIN(libpmemobj_secure); // The exact layout name here does not matter
-POBJ_LAYOUT_ROOT(libpmemobj_secure, struct pmemobj_asan_root);
-POBJ_LAYOUT_TOID(libpmemobj_secure, struct pmemobj_asan_shadowmem);
-POBJ_LAYOUT_TOID(libpmemobj_secure, struct pmemobj_asan_end); // A special type marking the highest type number used by SPMO
-POBJ_LAYOUT_END(libpmemobj_secure);
-
-struct pmemobj_asan_shadowmem {}; // A dummy type, whose type number is used for the shadow memory.
-
-struct pmemobj_asan_root {
-	PMEMoid shadow_mem;
-	uint64_t pool_size;
-	PMEMoid real_root;
-	uint64_t real_root_size;
-};
-
-struct pmemobj_asan_end {};
 
 uint8_t* pmemobj_asan_get_shadow_mem_location(void* _p);
 
--- src/libpmemobj/asan_wrappers.h
+++ src/libpmemobj/asan_wrappers.h
@@ -2,5 +2,24 @@
 #define LIBPMEMOBJ_ASAN_WRAPPERS_H
 
 #include "asan.h"
+#include <libpmemobj/types.h>
+#include "tx.h"
+
+POBJ_LAYOUT_BEGIN(libpmemobj_secure); // The exact layout name here does not matter
+POBJ_LAYOUT_ROOT(libpmemobj_secure, struct pmemobj_asan_root);
+POBJ_LAYOUT_TOID(libpmemobj_secure, struct pmemobj_asan_shadowmem);
+POBJ_LAYOUT_TOID(libpmemobj_secure, struct pmemobj_asan_end); // A special type marking the highest type number used by SPMO
+POBJ_LAYOUT_END(libpmemobj_secure);
+
+struct pmemobj_asan_shadowmem {}; // A dummy type, whose type number is used for the shadow memory.
+
+struct pmemobj_asan_root {
+	PMEMoid shadow_mem;
+	uint64_t pool_size;
+	PMEMoid real_root;
+	uint64_t real_root_size;
+};
+
+struct pmemobj_asan_end {};
 
 #endif
